21d3d1c1699122d23795331aef35587a101cf630
splewsh <stovonson@gmail.com>
2026-04-27 00:20:08 +0100

    Added Vercel adapter

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercelStatic from '@astrojs/vercel/static';
 
-// https://astro.build/config
-export default defineConfig({});
+import vercel from '@astrojs/vercel';
+
+export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
+});
